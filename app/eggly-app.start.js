5a922f17531044c0db49e9de3ed6900d7e2eeb2f
jmichelin <jmichelinphone@gmail.com>
2016-02-17 15:52:34 -0800

    finished lesson 3

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -2,5 +2,19 @@ angular.module('Eggly', [
 
 ])
 .controller('MainCtrl', function($scope){
-  $scope.hello = 'world';
+  $scope.categories = [
+    {"id": 0, "name": "Development"},
+    {"id": 1, "name": "Design"},
+    {"id": 2, "name": "Fellowship"},
+    {"id": 3, "name": "Best Practices"},
+    {"id": 4, "name": "Humor"},
+  ];
+  $scope.bookmarks = [
+    {"id": 0, "title": "AngularJS", "url": "https://angularjs.org/", "category": "Development"},
+    {"id": 1, "title": "Interview Prompts", "url": "http://api.hackreactor.com/prompts/", "category": "Fellowship"},
+    {"id": 2, "title": "UX Booth - Best Practices", "url": "http://www.uxbooth.com/articles/making-and-breaking-ux-best-practices/", "category": "Best Practices"},
+    {"id": 3, "title": "AngularJS Fundamentals", "url": "https://egghead.io/series/angularjs-app-from-scratch-getting-started", "category": "Development"},
+    {"id": 4, "title": "Corgi", "url": "http://www.humorhound.com/tag/corgi-2/", "category": "Humor"},
+    {"id": 5, "title": "Information is Beautiful", "url": "http://www.informationisbeautiful.net/", "category": "Design"}
+  ]
 });
