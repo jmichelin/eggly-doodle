b05361b6b3707a11c38cd47adcee0b7a8cc92b63
jmichelin <jmichelinphone@gmail.com>
2016-03-03 11:58:59 -0800

    test

---
+++ app/categories/bookmarks/create/bookmark-create.js
@@ -0,0 +1,11 @@
+angular.module('categories.bookmarks.create', [
+
+])
+    .config(function($stateProvider){
+        $stateProvider
+            .state('eggly.categories.bookmarks.create', {
+                url: '/bookmarks/create',
+                templateUrl: 'app/categories/bookmarks/create/',
+            })
+    })
+;
