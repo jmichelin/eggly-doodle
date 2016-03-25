a8c29b7a1295409f0dcb10ca557ce166927b0a97
jmichelin <jmichelinphone@gmail.com>
2016-03-25 13:40:13 -0700

    [chore] rename index.html

---
+++ bindingSetup.js
@@ -0,0 +1,41 @@
+/**
+ * Created by jmichelin on 3/18/16.
+ * For creating live templates
+ */
+var one = $q.defer();
+var two = $q.defer();
+var three = $q.defer();
+
+function() {
+
+}
+
+function () {
+    
+    const two2 = two;
+    two2.resolve();
+}
+function fixit() {
+
+}
+
+var anotherTest2 = anotherTest;
+
+
+fixit
+
+all.then(fixit)
+console.log();
+
+console.log('three this', thshow this);
+
+console.log('this', this);
+
+console.log('this', this);
+
+
+var message = '';
+console.log(
+    almessage + 's', this);
+console.log('this', this);
+console.log('this', this);
