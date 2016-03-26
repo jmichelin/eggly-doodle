5318a97106daa073c16b85052f3b923c0c3f6c3f
jmichelin <jmichelinphone@gmail.com>
2016-03-25 17:29:34 -0700

    finishes lesson 5

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
  */
 var gulp = require('gulp'),
     del = require('del'),
-    runSequence = require('run-sequence');
+    runSequence = require('run-sequence'),
+    inject = require('gulp-inject');
 
 //set default task
 gulp.task('default', function(callback){
@@ -13,15 +14,39 @@ gulp.task('default', function(callback){
 gulp.task('build', function (callback) {
     runSequence('clean',
         'copy-build',
+        'index',
         callback);
+});
+
+gulp.task('index', function () {
+    var tpl_src = ['./build/vendor/**/*.js',
+    './build/app/**/*.js',
+    './build/assets/css/**/*.css'];
+
+    return gulp.src('./src/index.html')
+        .pipe(inject(gulp.src(tpl_src), {ignorePath: 'build'}))
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('clean', function () {
     del(['./build'], {force: true});
 });
 
-gulp.task('copy-build', ['copy-assets', 'copy-app-js', 'copy-vendor-js'], function(){
+gulp.task('copy-build', ['copy-html', 'copy-json', 'copy-assets', 'copy-app-js', 'copy-vendor-js'], function(){
     console.log("inside copy build");
+});
+
+//intermediate step
+gulp.task('copy-html', function () {
+    return gulp.src(['./src/**/*.html',
+        '!./src/index.html'])
+        .pipe(gulp.dest('./build'));
+});
+
+//intermediate step
+gulp.task('copy-json', function () {
+    return gulp.src('./src/**/*.json')
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('copy-assets', function () {
